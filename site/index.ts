b8e62d7917ce7f2da16fc6b72bf44cbd52f6bc94
keroxp <kerokerokerop@gmail.com>
2019-09-23 13:57:59 +0900

    bump: deno@v0.18.0, deno_std@v0.18.0

--- site/index.ts
+++ site/index.ts
@@ -1,8 +1,8 @@
 import { createRouter } from "../router.ts";
 import { serveStatic } from "../serve_static.ts";
-
+import * as path from "../vendor/https/deno.land/std/fs/path.ts";
 const router = createRouter();
 const port = Deno.env()["PORT"] || "8899";
-router.use(serveStatic("./public"));
+router.use(serveStatic(path.resolve(import.meta.url, "./public")));
 router.listen(":" + port);
 console.log("servest-site: running on :" + port);
--- vendor/https/deno.land/std/textproto/mod.ts
+++ vendor/https/deno.land/std/textproto/mod.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@v0.17.0/textproto/mod.ts";
+export * from "https://deno.land/std@v0.18.0/textproto/mod.ts";
