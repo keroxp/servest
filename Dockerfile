96e8bcd778e9f845d6345862123ea2e4749a2654
Yusuke Sakurai <kerokerokerop@gmail.com>
2019-10-08 21:39:56 +0900

    feat: use jsx for site (#31)
    - feat: use jsx for site
    - bump: deno@v0.20.0 for CI and site

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM debian:stretch
 RUN apt update -y && apt install curl -y
-ARG DENO_VERSION=v0.19.0
+ARG DENO_VERSION=v0.20.0
 RUN curl -fsSL https://deno.land/x/install/install.sh | sh -s -- ${DENO_VERSION}
 ENV DENO_INSTALL=/root/.deno
 ENV PATH=${DENO_INSTALL}/bin:${PATH}
@@ -10,4 +10,4 @@ WORKDIR /servest/site
 ENV PORT=${PORT}
 EXPOSE ${PORT}
 RUN cp *.ts /servest/site/public
-CMD ["deno", "--allow-net", "--allow-read", "--allow-env", "index.ts"]
+CMD ["deno", "--allow-net", "--allow-read", "--allow-env", "index.tsx"]
--- benchmark/main.ts
+++ benchmark/main.ts
@@ -1,8 +1,8 @@
 export async function main() {
   const tgt = Deno.env()["TARGET"];
-  console.log("start benching for "+tgt);
-  await Deno.run({args: ["deno", "fetch", tgt ]}).status();
-  Deno.run({args: ["deno", "-A", tgt]});
+  console.log("start benching for " + tgt);
+  await Deno.run({ args: ["deno", "fetch", tgt] }).status();
+  Deno.run({ args: ["deno", "-A", tgt] });
   await new Promise(resolve => setTimeout(resolve, 1000));
   const p2 = Deno.run({
     args: ["wrk", "-t12", "-c400", "-d30s", "http://127.0.0.1:4500"]
