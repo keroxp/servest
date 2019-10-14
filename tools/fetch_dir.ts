7710a4978aae8560419384d0d99974b054c2dc9a
keroxp <kerokerokerop@gmail.com>
2019-10-15 04:45:35 +0900

    fix: [site] Dockerfile
    - speed up for building and importing dynamic

---
+++ tools/fetch_dir.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env deno -A
+import * as fs from "../vendor/https/deno.land/std/fs/mod.ts";
+import * as path from "../vendor/https/deno.land/std/fs/path.ts";
+export async function fetchDir(dir: string): Promise<number> {
+  let codes: string[] = [];
+  const glob = path.join(dir, "**/*.ts*");
+  for await (const { filename } of fs.expandGlob(glob)) {
+    if (!filename.endsWith(".d.ts") && filename.match(/\.tsx?$/)) {
+      codes.push('import "' + filename + '"');
+    }
+  }
+  const encoder = new TextEncoder();
+  const tempDir = await Deno.makeTempDir({ prefix: "servest" });
+  const tempFile = path.resolve(tempDir, "_mod.ts");
+  await Deno.writeFile(tempFile, encoder.encode(codes.join("\n")));
+  const proc = await Deno.run({
+    args: [Deno.execPath(), "fetch", path.resolve(tempFile)]
+  });
+  const { code } = await proc.status();
+  await Deno.remove(tempDir, { recursive: true });
+  return code === undefined ? 1 : code;
+}
+
+async function main() {
+  const code = await fetchDir(Deno.args[1]);
+  Deno.exit(code);
+}
+
+if (import.meta.main) {
+  main();
+}
