3a10161bbb214f04e337c0e851b8901e053be35e
keroxp <kerokerokerop@gmail.com>
2019-10-31 22:09:03 +0900

    reorg: remote test.ts

--- test.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env deno -A
-import { runTestModules } from "./vendor/https/deno.land/std/testing/runner.ts";
-
-runTestModules({
-  include: ["*_test.ts"]
-}).finally(() => {
-  Deno.exit(0);
-});
