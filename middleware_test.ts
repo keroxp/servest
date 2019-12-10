79081bf75b3ef0ed5a6e56669c633d592cdd8602
Yusuke Sakurai <kerokerokerop@gmail.com>
2019-12-10 11:07:42 +0900

    fix: [middleware] methodFilter throw error

---
+++ middleware_test.ts
@@ -0,0 +1,24 @@
+import { methodFilter } from "./middleware.ts";
+import { it } from "./test_util.ts";
+import {
+  assertEquals,
+  assertThrowsAsync
+} from "./vendor/https/deno.land/std/testing/asserts.ts";
+import { runIfMain } from "./vendor/https/deno.land/std/testing/mod.ts";
+import { createRecorder } from "./testing.ts";
+import { IncomingHttpRequest } from "./server.ts";
+import { RoutedServerRequest } from "./router.ts";
+import { RoutingError } from "./error.ts";
+it("middleware", t => {
+  t.run("basic", async () => {
+    const filter = methodFilter("POST");
+    const req = createRecorder({
+      url: "/",
+      method: "GET"
+    });
+    await assertThrowsAsync(async () => {
+      await filter(req);
+    }, RoutingError);
+  });
+});
+runIfMain(import.meta);
