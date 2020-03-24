99c61ee3acda3504970e52061ebc9e045b91f429
keroxp <kerokerokerop@gmail.com>
2020-03-25 00:02:42 +0900

    bump: react@v16.13

--- vendor/https/dev.jspm.io/react-dom/server.js
+++ vendor/https/dev.jspm.io/react-dom/server.js
@@ -1,7 +1,5 @@
 // @deno-types="../../../../types/react-dom/server/index.d.ts"
-export * from "https://dev.jspm.io/react-dom@16.10.2/server.js";
+export * from "https://dev.jspm.io/react-dom@16.13/server.js";
 // @deno-types="../../../../types/react-dom/server/index.d.ts"
-import {
-  default as dew
-} from "https://dev.jspm.io/react-dom@16.10.2/server.js";
+import { default as dew } from "https://dev.jspm.io/react-dom@16.13/server.js";
 export default dew;
