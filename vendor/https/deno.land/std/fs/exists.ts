4061f173d643a1e9282327feaa19985db835fb61
Yusuke Sakurai <kerokerokerop@gmail.com>
2020-05-15 00:36:53 +0900

    bump: deno@v1 (#112)

--- vendor/https/deno.land/std/fs/exists.ts
+++ vendor/https/deno.land/std/fs/exists.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@v0.50.0/fs/exists.ts";
+export * from "https://deno.land/std@v0.51.0/fs/exists.ts";
--- vendor/https/deno.land/std/http/http_status.ts
+++ vendor/https/deno.land/std/http/http_status.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@v0.50.0/http/http_status.ts";
+export * from "https://deno.land/std@v0.51.0/http/http_status.ts";
