cbfc5628c5d59babeb68c8f1b43f3773bf7faa72
Yusuke Sakurai <kerokerokerop@gmail.com>
2020-05-26 01:15:01 +0900

    site: add comparisons

---
+++ site/public/example/comparison/body/std_http.ts
@@ -0,0 +1,20 @@
+import {
+  serve,
+} from "https://deno.land/std/http/mod.ts";
+
+const server = serve({ port: 8888 });
+for await (const req of server) {
+  if (
+    req.method === "POST" &&
+    req.url === "/post"
+  ) {
+    const buf = new Deno.Buffer();
+    await Deno.copy(req.body, buf);
+    const decoder = new TextDecoder();
+    const str = decoder.decode(buf.bytes());
+    const json = JSON.parse(str);
+    // handling...
+  } else {
+    req.respond({ status: 404 });
+  }
+}
---
+++ site/public/example/comparison/ws/servest.ts
@@ -0,0 +1,12 @@
+import {
+  createApp,
+} from "../../../../../mod.ts";
+
+const app = createApp();
+app.ws("/ws", async (sock) => {
+  for await (const msg of sock) {
+    if (typeof msg === "string") {
+      // handle messages...
+    }
+  }
+});
