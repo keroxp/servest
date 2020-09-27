4d47ea5e3f95ffc429fc58365cc364307a7a5915
Yusuke Sakurai <kerokerokerop@gmail.com>
2020-09-27 15:51:37 +0900

    bump: deno@v1.4.2 (#140)

--- vendor/https/deno.land/std/fs/expand_glob.ts
+++ vendor/https/deno.land/std/fs/expand_glob.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.70.0/fs/expand_glob.ts";
+export * from "https://deno.land/std@0.71.0/fs/expand_glob.ts";
--- vendor/https/deno.land/std/io/writers.ts
+++ vendor/https/deno.land/std/io/writers.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.70.0/io/writers.ts";
+export * from "https://deno.land/std@0.71.0/io/writers.ts";
