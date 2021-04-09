4e1cb2f4329679298a9cd6bfca5629f25e1d5715
Yusuke Sakurai <kerokerokerop@gmail.com>
2021-04-09 19:21:55 +0900

    Update _adapter.ts

--- _adapter.ts
+++ _adapter.ts
@@ -1,3 +1,4 @@
+// Copyright 2019-2020 Yusuke Sakurai. All rights reserved. MIT license.
 import { createBodyParser } from "./body_parser.ts";
 import { readRequest, writeResponse } from "./serveio.ts";
 import {
